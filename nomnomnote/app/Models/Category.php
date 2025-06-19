479961cf720ee704a570335fb3a550f8fb947fce
Maximilian Huber <maxiboy44yt@gmail.com>
2025-06-19 12:35:31 +0200

    feat: create Category Model and migration

---
+++ nomnomnote/app/Models/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'image',
+        'description',
+        'created_by',
+    ];
+
+    /**
+     * Get the user who created the category.
+     */
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
--- nomnomnote/app/Models/User.php
+++ nomnomnote/app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class, 'created_by');
+    }
+
 }
